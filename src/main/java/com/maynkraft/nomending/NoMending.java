d55fb2126963a5c594d7e8916ca4471d5f419bbc
batikan <bgencacat@gmail.com>
2026-03-27 21:27:23 +0300

    Initialize message updated.

--- src/main/java/com/maynkraft/nomending/NoMending.java
+++ src/main/java/com/maynkraft/nomending/NoMending.java
@@ -19,6 +19,6 @@ public class NoMending implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("NoMending mod initialized.");
+		LOGGER.info("NoMending has been initialized.");
 	}
 }
